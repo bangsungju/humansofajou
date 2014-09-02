251bf9b056fd4f670991efef57f5746df129fe0f
wonjoo9315 <wonjoo9315@gmail.com>
2014-09-03 03:12:36 +0900

    배경 블러처리 수정, 전반적인 기타 디자인 수정, bg img 추가, ( nav 바 디자인 수정사항 아직 없음 - 수정하던 중 꼬여서 정리가 안됨.. )

--- HumanOfAjou/apps/static/js/style.js
+++ HumanOfAjou/apps/static/js/style.js
@@ -15,21 +15,32 @@
 // }
 
 
-$(document).ready(function(){
-	$(window).on('scroll', function() {
-		console.log($(window).scrollTop());
-		if($(window).scrollTop() < 450) {
-			var $scrollTop = $(window).scrollTop();
-			var $scrollPoint = 450;
+// $(document).ready(function(){
+// 	$(window).on('scroll', function() {
+// 		console.log($(window).scrollTop());
+// 		if($(window).scrollTop() < 450) {
+// 			var $scrollTop = $(window).scrollTop();
+// 			var $scrollPoint = 450;
 
-			var $per = 20 * ($scrollTop / $scrollPoint);
-			console.log($per);
+// 			var $per = 20 * ($scrollTop / $scrollPoint);
+// 			console.log($per);
 
-			$("#background-image").attr('style','-moz-' + $per + 'filter: blur(' + $per + 'px); -webkit-filter: blur(' + $per + 'px); -ms-filter: blur(' + $per + 'px); filter: blur(' + $per + 'px);');
-		}
-	});
-	
-});
+// 			$("#background-image").attr('style','-moz-' + $per + 'filter: blur(' + $per + 'px); -webkit-filter: blur(' + $per + 'px); -ms-filter: blur(' + $per + 'px); filter: blur(' + $per + 'px);');
+// 		}
+// 	});
+
+// });
+
+
+$(window).scroll(function() {
+      // get scroll position
+      var s = $(window).scrollTop();
+      // scroll value and opacity
+      opacityVal = (s / 280.0);
+      // opacity value 0% ~ 100%
+      $('.blurred-img').css('opacity', opacityVal);
+    });
+
 
 // navbar fixed by scroll
 $( window ).bind( 'scroll', function(){
@@ -49,7 +60,7 @@ $( window ).bind( 'scroll', function(){
     $( '#Header_nav_bar' ).css({ position :'relative', top : '0px',right:'0px',left:'0px'});
     $( '#Header_nav_bar li' ).css({ float: 'left'});
     $( '#Header_nav_bar li' ).css({ width: '96px'});
- 
+
     
     $( '.Prev_detail').css({position:'relative'});
     $( '.Next_detail').css({position:'relative'});
